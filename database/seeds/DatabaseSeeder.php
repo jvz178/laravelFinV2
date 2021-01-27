205254543613aca7a5488f95f54cce01b7164ad1
jvz178 <villar.zajua20@cadiz.salesianos.edu>
2021-01-27 19:38:00 +0100

    databaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(\App\Ciclo::class, 5)->create();
+        factory(\App\Noticia::class, 25)->create();
+        factory(\App\Oferta::class, 80)->create();
+        factory(\App\Requisito::class, 10)->create();
+        factory(\App\User::class, 50)->create();
+        factory(\App\Aplica::class, 30)->create();
+        factory(\App\Failed_Job::class, 20)->create();
     }
 }
